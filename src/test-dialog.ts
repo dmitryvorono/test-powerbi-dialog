826719aa598668e0dd70e12043d4092649ba5904
dmitryvorono <voronov-d88@yandex.ru>
2021-06-21 15:19:58 +0300

    create the dialog imp. file

---
+++ src/test-dialog.ts
@@ -0,0 +1,19 @@
+import powerbi from "powerbi-visuals-api";
+import DialogConstructorOptions = powerbi.extensibility.visual.DialogConstructorOptions;
+import DialogAction = powerbi.DialogAction;
+
+export class TestDialog {
+    static id = "MyTestDialog";
+    private target: HTMLElement;
+    private host;
+    constructor(options: DialogConstructorOptions, initialState: object) {
+        this.host = options.host;
+        this.target = options.element;
+        // … dialog rendering implementation …
+        const div = document.createElement('div');
+        div.className = 'test';
+        let text = document.createTextNode('Test');
+        div.appendChild(text);
+        this.target.appendChild(div);
+    }
+}
